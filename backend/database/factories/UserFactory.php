768fb556d2d2b3ff1d2b4fff6c5c87ed58c0bfcc
Josvany Hernández Ortega <josvanyho@gmail.com>
2026-03-05 14:49:12 -0500

    ok

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -21,17 +21,20 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+  public function definition(): array
 {
+    $faker = \Faker\Factory::create();
+
     return [
-        'name' => $this->faker->name(),
-        'email' => $this->faker->unique()->safeEmail(),
+        'name' => $faker->name(),
+        'email' => $faker->unique()->safeEmail(),
         'email_verified_at' => now(),
         'password' => static::$password ??= Hash::make('password'),
         'remember_token' => Str::random(10),
         'role' => 'viewer',
     ];
 }
+
 
     /**
      * Indicate that the model's email address should be unverified.
