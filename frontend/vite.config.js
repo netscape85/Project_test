61dab5f3c4ec7e14ffe875681025773d7ab2a03f
Josvany Hernández Ortega <josvanyho@gmail.com>
2026-03-05 14:33:31 -0500

    update despliegue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
     vuetify({ autoImport: true }),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['test.censa.edu.cu'],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
